4ca866e9d44917314ad7b2039fba8f5034d8cc55
Kristina T <kikitiki1989@gmail.com>
2023-02-19 13:23:02 +0100

    test: registration test

--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -1,10 +1,10 @@
 import { Link } from "react-router-dom";
 import Header from "../Header";
 
-export default function IndexPage(){
-    return (
-       <div>
-        index page goes here
-       </div>
-    );
+export default function IndexPage() {
+  return (
+    <div>
+      index page goes here asdf fdsa a asdfdsa fdsa fdsa fdsa fdsa fdsa dsaf as
+    </div>
+  );
 }
