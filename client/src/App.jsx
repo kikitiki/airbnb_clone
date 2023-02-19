4fad9577f39ae82ee4c922c38d59b10a15087882
Kristina T <kikitiki1989@gmail.com>
2023-02-19 13:20:33 +0100

    feat: initialize client

---
+++ client/src/App.jsx
@@ -0,0 +1,27 @@
+import axios from "axios";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Layout from "./Layout";
+import IndexPage from "./pages/IndexPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+
+const baseUrl = import.meta.env.VITE_BASE_URL123;
+
+axios.defaults.baseURL = baseUrl;
+
+console.log("AAAA", baseUrl);
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<IndexPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />}></Route>
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
---
+++ client/vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+ 
+  plugins: [react()],
+
+
+
+  // define: {
+  //   // By default, Vite doesn't include shims for NodeJS/
+  //   // necessary for segment analytics lib to work
+  //   // 'process.env.VITE_OPS': {},
+  //   global: {},
+    
+    
+  //   // 'process.env': {}
+  // },
+  
+})
+
